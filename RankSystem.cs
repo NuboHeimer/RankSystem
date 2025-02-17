18d46c18844490cbdbf04e31f295b6350b47b6a9
NuboHeimer <NuboHeimer@yandex.ru>
2025-02-17 10:50:43 +0300

    поднял версию

--- RankSystem.cs
+++ RankSystem.cs
@@ -4,7 +4,7 @@
 ///   Email:        nuboheimer@yandex.ru
 ///----------------------------------------------------------------------------
  
-///   Version:      0.2.1
+///   Version:      0.2.2
 using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
